d9c9543e85debfff8e9a73bf6f0e5d1522e6ab15
selvinkuik <selvinkuik@gmail.com>
2022-01-14 12:44:44 +0000

    Added simply-static-1-1642163624

---
+++ wp-content/plugins/simply-static-pro/assets/ssp-single-admin.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function($){$('#generate-single').on('click',function(){var single_id=$(this).attr('data-id');$.ajax({type:'POST',url:ssps_ajax.ajax_url,data:{'action':'apply_single','nonce':ssps_ajax.run_single_nonce,'single_id':single_id},dataType:'json',success:function(response){if(response.success){window.location.replace(ssps_ajax.redirect_url);}},});});});
